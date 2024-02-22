d20d18d0bf413b4b4bd138d1351bd6db6af05585
Mubin <mubinsyedmuhammed8@gmail.com>
2024-02-22 18:33:45 +0530

    initial commit

---
+++ vivi_frnd/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vivi_frnd project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vivi_frnd.settings")
+
+application = get_asgi_application()
